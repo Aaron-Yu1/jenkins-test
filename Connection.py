03ee5fd713f9bd8ede93429804c02d194823cded
root <peizhi.yu@hotmail.com>
2020-06-11 14:43:21 +0800

    No

--- Connection.py
+++ Connection.py
@@ -1,11 +1,12 @@
 import pymysql
 
+# If you want to use an interactive approach, you can use them
 # DB_Server = input("Please enter server name or IP of the DB Server: ")
 # DB_Name = input("Please enter the name of the DB Server: ")
 # DB_User = input("Please enter a user name you want connection to hte DB: ")
 # DB_Password = input("Please enter the password of the user: ")
 
-DB_Server = "15.83.246.104"
+DB_Server = "testdbsvr.sh.local"
 DB_Name = "db"
 DB_User = "admin"
 DB_Password = "Shanghai2010"
